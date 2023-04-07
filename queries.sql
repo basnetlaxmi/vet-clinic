7b4a9ccb950ec4cd5ebbc06be45b72eb0c65a722
basnetlaxmi <ddsanzel@gmail.com>
2023-04-08 00:25:46 +0545

    Add queries to queries file

--- queries.sql
+++ queries.sql
@@ -25,3 +25,15 @@ UPDATE animals SET weight_kg = weight_kg * -1;
  UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 -- Commit transaction
 COMMIT;
+-- How many animals are there?
+ SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+ SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS total_escapes FROM animals GROUP BY neutered ORDER BY total_escapes DESC LIMIT 1;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS min_wt, MAX(weight_kg) AS max_wt FROM animals GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+ SELECT species, AVG(escape_attempts) as avg_escapes FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2002-12-31' GROUP BY species;
