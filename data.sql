151ff709cccc514a943c5511779d1512366aab15
basnetlaxmi <ddsanzel@gmail.com>
2023-04-08 08:42:23 +0545

    Add update queries in data

--- data.sql
+++ data.sql
@@ -4,7 +4,6 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon','2018-11-15', 2, true, 8);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu','2021-01-07', 1, false, 15.04);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon','2017-05-12', 5, true, 11);
-
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Charmander','2020-02-08',0,false,-11,'');
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Plantmon','2021-11-15',2,true,-5.7,'');
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Squirtle','1993-04-02',3,false,-12.13,'');
@@ -12,3 +11,9 @@ INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,specie
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Boarmon','2005-06-07',7,true,20.4,'');
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Blossom','1998-10-13',3,true,17,'');
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES ('Ditto','2022-05-14',4,true,22,'');
+
+UPDATE animals SET owner_id = owners.id FROM owners WHERE owners.full_name = 'Sam Smith' AND animals.name ='Agumon';
+UPDATE animals SET owner_id = owners.id FROM owners WHERE owners.full_name = 'Jennifer Orwell' AND animals.name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = owners.id FROM owners WHERE owners.full_name = 'Bob' AND animals.name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = owners.id FROM owners WHERE owners.full_name = 'Melody Pond' AND animals.name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = owners.id FROM owners WHERE owners.full_name = 'Dean Winchester' AND animals.name IN ('Angemon','Boarmon');
